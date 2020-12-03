7d1ed79c548d7f0e8e8870cca91f3ff2d50ed435
praweenaka Ayeshmantha <praweenakahemachandra@gmail.com>
2020-12-03 23:42:29 +0530

    up

--- serach_customer.php
+++ serach_customer.php
@@ -23,9 +23,7 @@ include_once './connection_sql.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
 
-
-
-    <!-- <script language="JavaScript" src="js/search_joborder.js"></script> -->
+ 
 
     <script language="JavaScript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script language="JavaScript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
