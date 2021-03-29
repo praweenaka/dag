e497966eef1b0faea7afa5157d5fb5d8966a5a10
praweenaka Ayeshmantha <praweenakahemachandra@gmail.com>
2021-03-29 23:53:25 +0530

    up

--- creditnote_cus_search.php
+++ creditnote_cus_search.php
@@ -11,7 +11,7 @@ include_once './connection_sql.php';
     <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
 
 
-    <title>Search Customer Creditnote</title>
+    <title>Search Customer Credit Note</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
 
