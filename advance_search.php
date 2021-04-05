5aa1dc4bbeb7d5990b7f4d5b1789890ef60eefe8
praweenaka Ayeshmantha <praweenakahemachandra@gmail.com>
2021-04-05 23:59:28 +0530

    up

--- advance_search.php
+++ advance_search.php
@@ -11,7 +11,7 @@ include_once './connection_sql.php';
     <link href="style.css" rel="stylesheet" type="text/css" media="screen" />
 
 
-    <title>Search Advance</title>
+    <title>Search Advance List</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
 
