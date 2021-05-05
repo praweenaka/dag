68e6b89755d2d09296dfcc2821d551fcbd6fbfcf
praweenaka Ayeshmantha <praweenakahemachandra@gmail.com>
2021-05-05 23:42:21 +0530

    up

--- config.inc.php
+++ config.inc.php
@@ -7,12 +7,12 @@ define ("DB_SERVER","localhost");
 // Database server user name
 define ("DB_USER_NAME","coolmans");
 // Database Server password
-define ("DB_PASSWORD","e1E]0Mfua.0T4L");
+define ("DB_PASSWORD","");
 
 
  
 
-define ("DB_DATABSE","coolmans_dag");
+define ("DB_DATABSE","dag");
 
 
 ?>
