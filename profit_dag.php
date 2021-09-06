e208f06d7b1fc6b1e24ccc7bb37b69ab33bd98ef
praweenaka <praweenakahemachandra@gmail.com>
2021-09-06 23:46:59 +0530

    up

--- profit_dag.php
+++ profit_dag.php
@@ -29,7 +29,7 @@ require_once("./connection_sql.php");
 
     <div class="box box-primary">
         <div class="box-header with-border">
-            <h3 class="box-title">PROFIT REPORT</h3>
+            <h3 class="box-title">VIEW PROFIT REPORT</h3>
         </div>
         <form role="form" class="form-horizontal">
             <div class="box-body"> 
