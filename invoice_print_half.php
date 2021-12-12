87049b232e9730d6beb97556bf1008e73f73a44a
praweenaka <praweenakahemachandra@gmail.com>
2021-12-13 00:00:36 +0530

    up

--- invoice_print_half.php
+++ invoice_print_half.php
@@ -5,7 +5,7 @@
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
 
-    <title>Invoice Print</title>
+    <title>Invoice Print Half</title>
 
     <style>
         /*.center {*/
